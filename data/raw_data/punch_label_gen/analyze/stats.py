e8834873dc5aad5b803f8f0d8d90a9e0396a3fff
Chirag <chiragbhuvaneshwar@gmail.com>
2021-09-22 23:27:50 +0200

    updated punch stats calc

--- data/raw_data/punch_label_gen/analyze/stats.py
+++ data/raw_data/punch_label_gen/analyze/stats.py
@@ -1,8 +1,28 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from common.utils import retrieve_name
+
+
+def get_bins(min_max):
+    d = min_max[1] - min_max[0]
+    num_bins = 6
+    step = d / num_bins
+    bins = [round(min_max[0] + step * i, 2) for i in range(num_bins)]
+    return np.array(bins)
+
+
+def get_binned_arr(arr, arr_min_max):
+    bins = get_bins(arr_min_max)
+    bin_idx = np.digitize(arr, bins)
+
+    binned_arr = []
+    for idx in bin_idx:
+        binned_arr.append(bins[idx - 1])
+    binned_arr = np.array(binned_arr)
+    return binned_arr, bin_idx
 
 
 def get_punch_count(punch_labels_df, hand, punch_val):
@@ -22,7 +42,7 @@ def get_punch_count(punch_labels_df, hand, punch_val):
     return count_array
 
 
-def gen_punch_stats(data_directory):
+def gen_punch_stats(data_directory, plot=False):
     count_r_fwd = np.array([])
     count_r_rev = np.array([])
     count_l_fwd = np.array([])
@@ -35,10 +55,48 @@ def gen_punch_stats(data_directory):
         count_l_fwd = np.append(count_l_fwd, get_punch_count(punch_phase_df, 'left punch', 1.0))
         count_l_rev = np.append(count_l_rev, get_punch_count(punch_phase_df, 'left punch', -1.0))
 
-    punch_count_print = [count_r_fwd, count_r_rev, count_l_fwd, count_l_rev]
+    count_r = list(np.array(count_r_fwd) + np.array(count_r_rev))
+    count_l = list(np.array(count_l_fwd) + np.array(count_l_rev))
+
+    r_vals, r_bin_idxs = get_binned_arr(count_r, [0, 60])
+    l_vals, l_bin_idxs = get_binned_arr(count_l, [0, 60])
+
+    r_unique, r_counts = np.unique(count_r, return_counts=True)
+    r_count_dict = {k: v for k, v in zip(r_unique, r_counts)}
+    l_unique, l_counts = np.unique(l_vals, return_counts=True)
+    l_count_dict = {k: v for k, v in zip(l_unique, l_counts)}
+
+    if plot:
+        plt.hist(count_r, density=False, bins=60)  # density=False would make counts
+        plt.ylabel('Num Punches')
+        plt.xlabel('Frames')
+        plt.title("Count of right punches in dataset")
+        plt.xlim(0, 60)
+        plt.ylim(0, 40)
+        plt.axvline(sum(count_r)/len(count_r), color='k', linestyle='--', label='Avg right punch ='+str(round(sum(count_r)/len(count_r))))
+        plt.legend()
+        plt.savefig("data/raw_data/punch_label_gen/analyze/right_punch_count.png")
+        plt.clf()
+
+        plt.hist(count_l, density=False, bins=60)  # density=False would make counts
+        plt.ylabel('Num Punches')
+        plt.xlabel('Frames')
+        plt.title("Count of left punches in dataset")
+        plt.xlim(0, 60)
+        plt.ylim(0, 40)
+
+        plt.axvline(sum(count_l) / len(count_l), color='k', linestyle='--',
+                    label='Avg left punch =' + str(round(sum(count_l) / len(count_l))))
+        plt.legend()
+        plt.savefig("data/raw_data/punch_label_gen/analyze/left_punch_count.png")
+        plt.clf()
+
+
+    punch_count_print = [count_r_fwd, count_r_rev, count_l_fwd, count_l_rev, count_r, count_l]
     names = list(map(retrieve_name, punch_count_print))
     punch_stats = []
 
+    avg_punch = []
     for i, p_count in enumerate(punch_count_print):
         p_count = np.array(p_count)
 
@@ -51,7 +109,11 @@ def gen_punch_stats(data_directory):
             "max_punch_duration": p_count.max()
         }
         punch_stats.append(dir_stats)
+        avg_punch.append(p_count.mean())
         print(dir_stats)
+
+    # print("Overall Average Punch Duration = ", round(np.sum(avg_punch[:4])/2))
+    print("Overall Average Punch Duration = ", round(np.sum(avg_punch[4:]) / 2))
 
     return punch_stats
 
@@ -60,4 +122,4 @@ if __name__ == '__main__':
     INPUT_BASE_PATH = os.path.join("data", "raw_data")
     data_dir = os.path.join(INPUT_BASE_PATH, "punch_label_gen", "punch_label", "tertiary")
     # data_dir = os.path.join(INPUT_BASE_PATH, "punch_label_gen", "punch_label", "binary")
-    gen_punch_stats(data_dir)
+    gen_punch_stats(data_dir, plot=True)
