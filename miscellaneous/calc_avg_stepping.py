28c9354c1e4febb6dd7a2187c28a53d32840edbe
Chirag <chiragbhuvaneshwar@gmail.com>
2021-11-06 22:59:50 +0100

    code added for average stepping info

--- miscellaneous/calc_avg_stepping.py
+++ miscellaneous/calc_avg_stepping.py
@@ -1,5 +1,40 @@
+import numpy as np
+import pandas as pd
+
 start_r = [289, 341]
-end_r =   [318, 364]
+end_r = [318, 364]
 
 start_l = [324, 368]
-end_l =   [340, 381]
+end_l = [340, 381]
+
+
+def calc_avg_step(st, end):
+    st = np.array(st)
+    end = np.array(end)
+
+    diff = end - st
+    mean = np.mean(diff)
+    return mean
+
+
+l_mean = calc_avg_step(start_l, end_l)
+r_mean = calc_avg_step(start_r, end_r)
+
+overall_mean = np.mean([l_mean, r_mean])
+
+# info = {
+#     "right": round(r_mean, 3),
+#     "left": round(l_mean, 3),
+#     "overall": round(overall_mean, 3)
+# }
+# df = pd.DataFrame.from_dict(info, orient='index', columns=["average duration"])
+
+step_type = ["right", "left", "overall"]
+step_durations = [round(r_mean,3), round(l_mean,3), round(overall_mean,3)]
+
+info = {
+    "step type": step_type,
+    "average": step_durations,
+}
+df = pd.DataFrame(info)
+print(df.to_latex(index=False))
